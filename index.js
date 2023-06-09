1c1468a3e2d39e2024e754c15008a330d881150a
samantariya <samantaislam355@gmail.com>
2023-06-09 12:09:34 +0600

    jwt token set done

--- index.js
+++ index.js
@@ -90,6 +90,7 @@ async function run() {
       const result = { admin: user?.role === "admin" };
       res.send(result);
     });
+
     app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
 
@@ -103,6 +104,34 @@ async function run() {
       const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
+
+    app.patch("/users/instructor/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "instructor",
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.get("/users/instructor/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({ instructor: false });
+      }
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { instructor: user?.role === "instructor" };
+      res.send(result);
+    });
+
     app.get("/reviews", async (req, res) => {
       const result = await reviewCollection.find().toArray();
       res.send(result);
